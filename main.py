e72a3fd93177e25e43ef203c8e5520104b927bb8
andressavilarinho <global-anvilgui@gmail.com>
2022-11-01 08:46:00 -0300

    first commit

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
+    return jsonify({"Mensagem": "Bom dia Andressa VIlarinho 🚅"})
 
 
 if __name__ == '__main__':
